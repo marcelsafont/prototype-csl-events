7fa9fb48bd451a79311ed87a24b045f31c5b9714
marcelsafont <marcel.safont1983@gmail.com>
2024-03-17 09:19:58 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `control-shift-events-prototype`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
